ec0f9de0f84dc47889270818c210f03e036074d2
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-07-01 17:20:16 +0000

    progress

--- makemeahanzi-clone/makemeahanzi-newer.js
+++ makemeahanzi-clone/makemeahanzi-newer.js
@@ -16,69 +16,88 @@ const VectorFunctions = {
 function orDefault(maybe_null, dflt) {
   return null == maybe_null ? dflt : maybe_null
 }
-function func1(n, r) {
-  var a = [], e = 0
-  for (var i = 0; i < n.length - 1; i++)
-    e += Math.sqrt(VectorFunctions.distance2(n[i], n[i + 1]))
-  var h = 0, o = n[0], u = 0
-  for (var i = 0; r - 1 > i; i++) {
-    for (var s = (i * e) / (r - 1); s > u;) {
-      var c = Math.sqrt(VectorFunctions.distance2(o, n[h + 1]))
-      if (s > u + c) (h += 1), (o = n[h]), (u += c)
+
+/** 
+ * Map point from pair0's aabb to pair1 aabb
+ * 
+ * @param {pair of ponits} pair0 
+ * @param {pair of ponits} pair1 
+ * @returns 
+ */
+function createNormilizedProjectFunction(pair0, pair1) {
+  var diff0 = VectorFunctions.subtract(pair0[1], pair0[0]),
+    diff1 = VectorFunctions.subtract(pair1[1], pair1[0]),
+    diffratio = [diff1[0] / diff0[0], diff1[1] / diff0[1]]
+  return point => [
+    Math.round(diffratio[0] * (point[0] - pair0[0][0]) + pair1[0][0]),
+    Math.round(diffratio[1] * (point[1] - pair0[0][1]) + pair1[0][1]),
+  ]
+}
+
+function get_aabb(strokes) {
+  var p_min = [1 / 0, 1 / 0]
+  var p_max = [-(1 / 0), -(1 / 0)]
+  return strokes.map(points => points.map(point => {
+    (p_min[0] = Math.min(p_min[0], point[0])),
+      (p_min[1] = Math.min(p_min[1], point[1])),
+      (p_max[0] = Math.max(p_max[0], point[0])),
+      (p_max[1] = Math.max(p_max[1], point[1]))
+  })),
+    [p_min, p_max]
+}
+
+/**
+ * 
+ * @param {points} stroke 
+ * @param {integer} how_many_points_to_sample 
+ * @returns 
+ */
+function downsample_stroke(stroke, how_many_points_to_sample) {
+  var result_stroke = []
+  var stroke_length = 0
+  for (var i = 0; i < stroke.length - 1; i++)
+    stroke_length += Math.sqrt(VectorFunctions.distance2(stroke[i], stroke[i + 1]))
+  var h = 0, point_cadidate = stroke[0], u = 0
+  for (var i = 0; how_many_points_to_sample - 1 > i; i++) {
+    for (var s = (i * stroke_length) / (how_many_points_to_sample - 1); s > u;) {
+      var c = Math.sqrt(VectorFunctions.distance2(point_cadidate, stroke[h + 1]))
+      if (s > u + c) (h += 1), (point_cadidate = stroke[h]), (u += c)
       else {
         var f = (s - u) / c;
-        (o = [
-          (1 - f) * o[0] + f * n[h + 1][0],
-          (1 - f) * o[1] + f * n[h + 1][1],
+        (point_cadidate = [
+          (1 - f) * point_cadidate[0] + f * stroke[h + 1][0],
+          (1 - f) * point_cadidate[1] + f * stroke[h + 1][1],
         ]),
           (u = s)
       }
     }
-    a.push(VectorFunctions.round(o))
+    result_stroke.push(VectorFunctions.round(point_cadidate))
   }
-  return a.push(n[n.length - 1]), a
+  result_stroke.push(stroke[stroke.length - 1])
+  return result_stroke
 }
-function func2(pair0 /* pair of ponits */, pair1) {
-  var a = VectorFunctions.subtract(pair0[1], pair0[0]),
-    e = VectorFunctions.subtract(pair1[1], pair1[0]),
-    i = [e[0] / a[0], e[1] / a[1]]
-  return point => [
-    Math.round(i[0] * (point[0] - pair0[0][0]) + pair1[0][0]),
-    Math.round(i[1] * (point[1] - pair0[0][1]) + pair1[0][1]),
-  ]
-}
-function func3(t) {
-  var n = [1 / 0, 1 / 0],
-    r = [-(1 / 0), -(1 / 0)]
-  return t.map(t => t.map(t => {
-    (n[0] = Math.min(n[0], t[0])),
-      (n[1] = Math.min(n[1], t[1])),
-      (r[0] = Math.max(r[0], t[0])),
-      (r[1] = Math.max(r[1], t[1]))
-  })),
-    [n, r]
-}
-function func4(n, r, a) {
-  n = n.map(VectorFunctions.round)
-  var e = VectorFunctions.subtract(n[1], n[0])
+
+function func4(stroke_aa, r, a) {
+  stroke_aa = stroke_aa.map(VectorFunctions.round)
+  var e = VectorFunctions.subtract(stroke_aa[1], stroke_aa[0])
   if (e[0] < 0 || e[1] < 0) throw e
   if (e[0] < a) {
     var i = Math.ceil((a - e[0]) / 2);
-    (n[0][0] -= i), (n[1][0] += i)
+    (stroke_aa[0][0] -= i), (stroke_aa[1][0] += i)
   }
   if (e[1] < a) {
     var i = Math.ceil((a - e[1]) / 2);
-    (n[0][1] -= i), (n[1][1] += i)
+    (stroke_aa[0][1] -= i), (stroke_aa[1][1] += i)
   }
   if (r > 0)
-    if (((e = VectorFunctions.subtract(n[1], n[0])), e[0] < e[1] / r)) {
+    if (((e = VectorFunctions.subtract(stroke_aa[1], stroke_aa[0])), e[0] < e[1] / r)) {
       var i = Math.ceil((e[1] / r - e[0]) / 2);
-      (n[0][0] -= i), (n[1][0] += i)
+      (stroke_aa[0][0] -= i), (stroke_aa[1][0] += i)
     } else if (e[1] < e[0] / r) {
       var i = Math.ceil((e[0] / r - e[1]) / 2);
-      (n[0][1] -= i), (n[1][1] += i)
+      (stroke_aa[0][1] -= i), (stroke_aa[1][1] += i)
     }
-  return n
+  return stroke_aa
 }
 function func5(n, h) {
   if (
@@ -87,14 +106,14 @@ function func5(n, h) {
   )
     throw new Error("Invalid medians list: " + JSON.stringify(n))
   var o = h.side_length,
-    u = func4(func3(n), h.max_ratio, h.min_width),
+    u = func4(get_aabb(n), h.max_ratio, h.min_width),
     s = [
       [0, 0],
       [h.side_length - 1, h.side_length - 1],
     ],
-    c = func2(u, s)
+    c = createNormilizedProjectFunction(u, s)
   return n.map(n => {
-    var a = func1(n.map(c), h.points),
+    var a = downsample_stroke(n.map(c), h.points),
       e = VectorFunctions.subtract(a[a.length - 1], a[0]),
       i = Math.atan2(e[1], e[0]),
       u = Math.round(((i + Math.PI) * o) / (2 * Math.PI)) % o,
